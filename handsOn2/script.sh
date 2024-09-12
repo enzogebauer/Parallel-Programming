8383cb7995e6560ce8081c42f41c936cd3dd642b
Enzo Gebauer <enzogebauer@MacBook-Air-de-Enzo.local>
2024-09-12 12:57:12 -0300

    Finish

--- handsOn2/script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for ((i=2; i<=64; i*=2))
-do
-  echo "Number of threads:" $i "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
-  OMP_NUM_THREADS="$i" ./image
-  echo
-done
--- handsOn2/start.sh
+++ handsOn2/start.sh
@@ -3,10 +3,6 @@
 for ((i=1; i<=8; i++))
 do
   echo "Number of threads:" $i "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="  
-  for ((j=1000; j<=80000; j+=1200))
-  do
-    OMP_NUM_THREADS="$i" ./integral "$j"
-    echo "Number of threads:" $i "Number of intervals:" $j
-  done
+  OMP_NUM_THREADS="$i" ./count_primes
   echo 
 done
