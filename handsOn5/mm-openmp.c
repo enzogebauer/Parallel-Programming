4b5bc14ee4da3a9b9b42354e4b219f01a2a8e53b
Enzo Gebauer <enzogebauer@MacBook-Air-de-Enzo.local>
2024-09-25 23:49:50 -0300

    add hands on 5

---
+++ handsOn5/mm-openmp.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+void writeResultsToFile(int size, double time_spend, int num_threads) {
+    FILE *output_file = fopen("./mm_openMP.txt", "a");
+    if (output_file == NULL) {
+        perror("Erro ao abrir o arquivo de saída");
+        return;
+    } 
+
+    fprintf(output_file, "%d %lf %d ", size, time_spend, num_threads);
+    if(size == 1000) fprintf(output_file, "\n");
+
+    fclose(output_file);
+}
+
+void initializeMatrix(int *matrix, int size)
+{
+  for (int i = 0; i < size; i++)
+    for (int j = 0; j < size; j++)
+      matrix[i * size + j] = rand() % (10 - 1) * 1;
+}
+
+void printMatrix(int *matrix, int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    for (int j = 0; j < size; j++)
+      printf("%d\t", matrix[i * size + j]);
+    printf("\n");
+  }
+  printf("\n");
+}
+
+int main (int argc, char **argv)
+{
+
+ int size = atoi(argv[1]);  
+ int i, j, k;
+ double t0, tf;
+
+ int  *A = (int *) malloc (sizeof(int)*size*size);
+ int  *B = (int *) malloc (sizeof(int)*size*size);
+ int  *C = (int *) malloc (sizeof(int)*size*size);
+
+ initializeMatrix(A, size);
+ initializeMatrix(B, size);
+
+ t0 = omp_get_wtime();
+
+  int num_threads = 0;
+
+ #pragma omp parallel for private(i, j, k)
+  for(i = 0; i < size; i++){
+    for(j = 0; j < size; j++){
+      for(k = 0; k < size; k++){
+        C[i * size + j] += A[i * size + k] * B[k * size + j];
+        if(i==0 && j==0 && k == 0)num_threads = omp_get_num_threads();
+      }     
+    }  
+  }
+ 
+ tf = omp_get_wtime();
+
+//printf("%d\t%f\n",size, tf-t0);
+
+// printMatrix(A,size);
+// printMatrix(B,size);
+// printMatrix(C,size);
+
+writeResultsToFile(size, tf-t0, num_threads);
+
+ return 0;
+
+}
